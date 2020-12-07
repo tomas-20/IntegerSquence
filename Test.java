17890f71cbd98c797f4136bf386a4accdd7547ba
tomas-20 <tacuna20@stuy.edu>
2020-12-07 13:06:53 -0500

    added some spaces that are of vital importance to the functioning of the program

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
 public class Test {
   public static void main(String[] arr) {
     IntegerSequence r = new Range(10, 20);
-    while(r.hasNext()) {
+    while (r.hasNext()) {
       System.out.print(r.next());
-      if(r.hasNext()) {
+      if (r.hasNext()) {
         System.out.print(", ");
       }
     }
