8151bb7b930164d60bcedd49e8094ed0aa5f1bf4
tomas-20 <tacuna20@stuy.edu>
2020-12-07 20:20:30 -0500

    made the ArraySequqnce functions

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,28 @@
 import java.util.NoSuchElementException;
-public class ArraySequence implements IntegerSequence{
+public class ArraySequence implements IntegerSequence {
   int currentIndex;
-  int []data;
+  int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other){
+    data = other;
+    currentIndex = 0;
+  }
+  public void reset() {
+    currentIndex = 0;
+  }
+  public int length() {
+    return data.length;
+  }
+  public boolean hasNext() {
+    return currentIndex < length();
+  }
+  public int next() {
+    if (!hasNext()) {
+      throw new NoSuchElementException("You cannot travel further through this array sequence");
+    }
+    int output = data[currentIndex];
+    currentIndex ++;
+    return output;
+  }
 }
--- Test.java
+++ Test.java
@@ -8,5 +8,12 @@ public class Test {
       }
     }
     System.out.println();
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+    System.out.println("ArraySequence(array):");
+    while (as.hasNext()) {
+      System.out.print(as.next() + ", ");
+    }
+    System.out.println();
   }
 }
