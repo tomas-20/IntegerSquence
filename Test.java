b6f178331c6760f210ddd1997c52a0a014d7bfcb
tomas-20 <tacuna20@stuy.edu>
2020-12-07 20:21:26 -0500

    made the tester fancy

--- Test.java
+++ Test.java
@@ -1,19 +1,18 @@
 public class Test {
-  public static void main(String[] arr) {
-    IntegerSequence r = new Range(10, 20);
-    while (r.hasNext()) {
-      System.out.print(r.next());
-      if (r.hasNext()) {
+  public static void print(IntegerSequence list) {
+    while (list.hasNext()) {
+      System.out.print(list.next());
+      if (list.hasNext()) {
         System.out.print(", ");
       }
     }
     System.out.println();
-    int[]nums = {1,3,5,0,-1,3,9};
+  }
+  public static void main(String[] arr) {
+    IntegerSequence r = new Range(10, 20);
+    print(r);
+    int[] nums = {1, 3, 5, 0, -1, 3, 9};
     IntegerSequence as = new ArraySequence(nums);
-    System.out.println("ArraySequence(array):");
-    while (as.hasNext()) {
-      System.out.print(as.next() + ", ");
-    }
-    System.out.println();
+    print(as);
   }
 }
